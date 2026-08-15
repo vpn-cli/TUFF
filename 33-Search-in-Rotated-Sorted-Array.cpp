e363c760cbdf15168baf57d81c7974e9e8c07c39
Vipin_k <112691594+vpn-cli@users.noreply.github.com>
2026-08-15 14:34:07 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 36.78%] - LeetPush

--- 33-Search-in-Rotated-Sorted-Array.cpp
+++ 33-Search-in-Rotated-Sorted-Array.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    int check(vector<int>& nums, int x) {
-        return int(nums[x] <= nums[nums.size() - 1]);
+    int check(vector<int>&nums, int y){
+        if (nums[y]<nums[0])return 1; // or nums[y]>nums[nums.size()-1] return 0;else return 1;
+        return 0;
     }
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int lo = 0;
-        int hi = n - 1;
-        int k = 0;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (check(nums, mid) == 0) {
-                lo = mid + 1;
-
-            } else {
-                k = mid;
-                hi = mid - 1;
+        int n= nums.size();
+        int lo=0;
+        int hi=n-1;
+        int k=0;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if (check(nums,mid)==0)lo=mid+1;
+            else{
+                k=mid;
+                hi=mid-1;
             }
         }
-        if (target > nums[n - 1]) {
-            lo = 0;
-            hi = k - 1;
-        } else {
-            lo = k;
-            hi = n - 1;
+        if (target>nums[n-1]){ // target is in the left side
+             lo=0;
+             hi=k-1;
         }
-        int ans = -1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] < target) {
-                lo = mid + 1;
-            } else {
-                ans = mid;
-                hi = mid - 1;
+        else{
+             lo=k;
+             hi=n-1;
+        }
+        int ans=-1;
+        while(lo<=hi){
+                int mid = lo+(hi-lo)/2;
+                if (target>nums[mid]){
+                    lo=mid+1;
+                }
+                else {
+                    ans=mid;
+                    hi=mid-1;
+                }
             }
-        }
-        return (ans >= 0 && ans < n && nums[ans] == target) ? ans : -1;
+        if (ans<n && ans>=0 && nums[ans]==target)return ans;
+        else return -1;
     }
 };
