205e54b97fadb5393598a977d8eda28cccbf18ad
Vipin_k <112691594+vpn-cli@users.noreply.github.com>
2026-08-20 00:59:29 +0530

    [3] [Time Beats: 17.33%] [Memory Beats: 20.74%] - LeetPush

--- 3-Longest-Substring-Without-Repeating-Characters.cpp
+++ 3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int r = -1;    
-        int l = 0;
-        map<int, int> mp;
         int n = s.size();
-        int temp = 0;
-        while (l < n) {
-            if (mp[s[r + 1]] == 0 && r + 1 < n) {  //checking if the right element exists or not
-                r++;
-                mp[s[r]]++;
-                temp = max(temp, r - l + 1);
-            } else {
+        int l = 0;
+        int ans = 0;
+        map<int, int> mp;
+        for (int r = 0; r < n; r++) { // initializing the window
+            mp[s[r]]++;
+            while (mp[s[r]] > 1) { // if the window is invalid
                 mp[s[l]]--;
                 l++;
             }
+            ans = max(ans, r - l + 1); // updating the answers
         }
-        return temp;
+        return ans;
     }
 };
